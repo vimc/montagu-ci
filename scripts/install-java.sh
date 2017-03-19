ecd0bc713c2321007c3c0eea7340e5b3363775ca
Rich FitzJohn <rich.fitzjohn@gmail.com>
2017-03-19 12:07:32 +0000

    Cache Java downloads across VMs

--- scripts/install-java.sh
+++ scripts/install-java.sh
@@ -1,12 +1,22 @@
 #!/bin/sh
+exit 0
+
 echo "Installing Java"
 set -x
-export JAVA_HOME=/usr/lib/jvm/java-8-oracle
+JAVA_HOME=/usr/lib/jvm/java-8-oracle
+JAVA_CACHE_VAGRANT=/vagrant/downloads/java
+JAVA_CACHE_SYSTEM=/var/cache/oracle-jdk8-installer
 
 if [ ! -d $JAVA_HOME ]; then
+    if [ -d $JAVA_CACHE_VAGRANT ]; then
+        rsync -av $JAVA_CACHE_VAGRANT/ `dirname $JAVA_CACHE_SYSTEM`
+    fi
+
     add-apt-repository -y ppa:webupd8team/java
     apt-get update
     echo debconf shared/accepted-oracle-license-v1-1 select true | debconf-set-selections
     echo debconf shared/accepted-oracle-license-v1-1 seen true | debconf-set-selections
     apt-get install -y oracle-java8-installer
+
+    rsync -av $JAVA_CACHE_SYSTEM $JAVA_CACHE_VAGRANT
 fi
