0c81add443332dcc141e5474c7ac879ed7a83264
Rich FitzJohn <rich.fitzjohn@gmail.com>
2017-03-20 09:14:59 +0000

    Fix java provisioning
    There was a stray exit left in from testing, and there is no need
    for the conditional now that we're using apt

--- scripts/install-java.sh
+++ scripts/install-java.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-exit 0
 
 echo "Installing Java"
 set -x
@@ -7,17 +6,17 @@ JAVA_HOME=/usr/lib/jvm/java-8-oracle
 JAVA_CACHE_VAGRANT=/vagrant/downloads/java
 JAVA_CACHE_SYSTEM=/var/cache/oracle-jdk8-installer
 
-if [ ! -d $JAVA_HOME ]; then
-    if [ -d $JAVA_CACHE_VAGRANT ]; then
-        rsync -av $JAVA_CACHE_VAGRANT/ `dirname $JAVA_CACHE_SYSTEM`
-    fi
-
-    add-apt-repository -y ppa:webupd8team/java
-    apt-get update
-    echo debconf shared/accepted-oracle-license-v1-1 select true | debconf-set-selections
-    echo debconf shared/accepted-oracle-license-v1-1 seen true | debconf-set-selections
-    apt-get install -y oracle-java8-installer
-
-    rsync -av $JAVA_CACHE_SYSTEM $JAVA_CACHE_VAGRANT
-    rm -r $JAVA_CACHE_SYSTEM
+if [ -d $JAVA_CACHE_VAGRANT ]; then
+    rsync -av $JAVA_CACHE_VAGRANT/ `dirname $JAVA_CACHE_SYSTEM`
 fi
+
+add-apt-repository -y ppa:webupd8team/java
+apt-get update
+echo debconf shared/accepted-oracle-license-v1-1 select true | \
+    debconf-set-selections
+echo debconf shared/accepted-oracle-license-v1-1 seen true | \
+    debconf-set-selections
+apt-get install -q -y oracle-java8-installer
+
+rsync -av $JAVA_CACHE_SYSTEM $JAVA_CACHE_VAGRANT
+rm -r $JAVA_CACHE_SYSTEM
