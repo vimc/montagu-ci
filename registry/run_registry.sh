0a918bed16a0b2419cdd1c89fdec5eeb525c53f5
Rich FitzJohn <rich.fitzjohn@gmail.com>
2017-04-04 10:47:11 +0100

    Use a persistent volume for storage

--- registry/run_registry.sh
+++ registry/run_registry.sh
@@ -22,6 +22,7 @@ fi
 echo "Starting docker registry"
 docker run -d -p 5000:5000 --restart=always --name registry \
        -v `pwd`/certs:/certs \
+       -v registry_data:/var/lib/registry \
        -e REGISTRY_HTTP_TLS_CERTIFICATE=$PATH_CRT \
        -e REGISTRY_HTTP_TLS_KEY=$PATH_KEY \
        registry:2
