bd89159cc610ce099f881afc9306921eedf9a2c2
Rich FitzJohn <rich.fitzjohn@gmail.com>
2017-04-04 10:51:16 +0100

    Split the docker registry provisioning out

---
+++ provision/setup-docker-registry.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -x
+
+REGISTRY_HOST=montagu.dide.ic.ac.uk:5000
+CERT_DEST=/etc/docker/certs.d/$REGISTRY_HOST
+CERT_SRC=/vagrant/files/agent/registry.crt
+
+if [ ! -f $CERT_SRC ]; then
+    echo "Certificate does not exist; see README, section 'docker registry'"
+    exit 1
+fi
+
+mkdir -p $CERT_DEST
+cp $CERT_SRC $CERT_DEST/domain.crt
+exit $?
--- provision/setup-docker.sh
+++ provision/setup-docker.sh
@@ -21,16 +21,3 @@ if getent passwd teamcity > /dev/null; then
         usermod -aG docker teamcity
     fi
 fi
-
-REGISTRY_HOST=fi--didelx05.dide.ic.ac.uk:5000
-CERT_DEST=/etc/docker/certs.d/$REGISTRY_HOST
-CERT_SRC=/vagrant/files/agent/registry.crt
-
-if [ ! -f $CERT_SRC ]; then
-    echo "Certificate does not exist; see README, section 'docker registry'"
-    exit 1
-fi
-
-mkdir -p $CERT_DEST
-cp $CERT_SRC $CERT_DEST/domain.crt
-exit $?
