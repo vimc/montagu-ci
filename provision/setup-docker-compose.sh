1b66c6d6f3c266e2b97f7ad9f5943313a835a8af
Rich FitzJohn <rich.fitzjohn@gmail.com>
2017-06-07 15:18:28 +0100

    Install docker-compose into /usr/bin to avoid path issues

--- provision/setup-docker-compose.sh
+++ provision/setup-docker-compose.sh
@@ -8,6 +8,6 @@ else
     echo "installing docker-compose"
     sudo curl -L \
          "https://github.com/docker/compose/releases/download/1.11.2/docker-compose-$(uname -s)-$(uname -m)" \
-         -o /usr/local/bin/docker-compose
-    sudo chmod +x /usr/local/bin/docker-compose
+         -o /usr/bin/docker-compose
+    sudo chmod +x /usr/bin/docker-compose
 fi
