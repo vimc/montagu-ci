73b9389aa5483f8fa1c2135dd6be3203dd0d31ba
Rich FitzJohn <rich.fitzjohn@gmail.com>
2017-10-10 16:15:30 +0100

    Script to write cron script

---
+++ scripts/write-cron-teamcity-backup-sync.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+if [ ! -f scripts/sync-backups.sh ]; then
+    echo "Run this from the montagu-ci directory"
+    exit 1
+fi
+
+PATH_VAGRANT=$PWD
+DEST=/etc/cron.daily/teamcity-backup-sync.sh
+
+cat <<EOF > $DEST
+#!/bin/sh
+cd ${PATH_VAGRANT} && ./scripts/sync-backups.sh
+EOF
+chmod +x $DEST
