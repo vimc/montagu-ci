3069104d41ab93fb137843c905598a5c9eadb22e
Rich FitzJohn <rich.fitzjohn@gmail.com>
2017-10-11 10:53:24 +0100

    Explanation for write-cron-teamcity-backup-sync.sh

--- scripts/write-cron-teamcity-backup-sync.sh
+++ scripts/write-cron-teamcity-backup-sync.sh
@@ -1,6 +1,19 @@
 #!/bin/sh
 set -e
 
+# The scripts/sync-backups.sh script must be run from the montagu-ci
+# directory because the backups will be accessed via the
+# montagu-ci/shared directory.  So before running the task we first
+# change to here and then run the script.  The construction below:
+#
+# 1. checks that the working directory is probably the right directory
+#    to run the sync script from
+#
+# 2. capture the absolute path to the working directory (as
+#    PATH_VAGRANT to avoid ambiguity about where the special variable
+#    PWD is referenced)
+#
+# 3. writes out the cron script including this path
 if [ ! -f scripts/sync-backups.sh ]; then
     echo "Run this from the montagu-ci directory"
     exit 1
