a8e25b194dc5a33db9f50604a30ff9c622b463f7
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2017-12-04 15:57:36 +0000

    Move docker out onto external disk

--- provision/setup-docker.sh
+++ provision/setup-docker.sh
@@ -6,6 +6,12 @@ if which -a docker > /dev/null; then
     echo "docker is already installed"
 else
     echo "installing docker"
+    # The big docker directory is /var/lib/docker - we'll move that
+    # out onto the external disk:
+    mkdir -p /mnt/data/docker
+    ln -s /mnt/data/docker/var-lib-docker /var/lib/docker
+
+    # Then continue with the installation
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
     sudo add-apt-repository \
          "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
