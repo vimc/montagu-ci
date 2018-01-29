268889ff769a900113c01705dd54f3aac5b9ebb2
Martin Eden <martin.s.eden@gmail.com>
2018-01-29 13:12:17 +0000

    Add comment

--- provision/setup-agent-users.sh
+++ provision/setup-agent-users.sh
@@ -9,5 +9,9 @@ TEAMCITY_GROUP=teamcity
 /usr/sbin/groupadd -r $TEAMCITY_GROUP 2>/dev/null
 /usr/sbin/useradd -c $TEAMCITY_USER -r -s /bin/bash -d $TEAMCITY_DIR -g $TEAMCITY_GROUP $TEAMCITY_USER 2>/dev/null
 
+# This grants the teamcity group permission to restart Docker
+# via sudo without needing a password. This is needed for the
+# Montagu repo build, which restarts the Docker daemon to simulate
+# a reboot.
 echo "%teamcity ALL=NOPASSWD: /bin/systemctl restart docker" \
 	| sudo tee /etc/sudoers.d/teamcity
