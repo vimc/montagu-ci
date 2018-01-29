67626036b515c1ff356f19ee0068145441cda938
Martin Eden <martin.s.eden@gmail.com>
2018-01-29 11:55:04 +0000

    Allow TeamCity agent to restart docker service.

--- provision/setup-agent-users.sh
+++ provision/setup-agent-users.sh
@@ -8,3 +8,6 @@ TEAMCITY_USER=teamcity
 TEAMCITY_GROUP=teamcity
 /usr/sbin/groupadd -r $TEAMCITY_GROUP 2>/dev/null
 /usr/sbin/useradd -c $TEAMCITY_USER -r -s /bin/bash -d $TEAMCITY_DIR -g $TEAMCITY_GROUP $TEAMCITY_USER 2>/dev/null
+
+echo "%teamcity ALL=NOPASSWD: /bin/systemctl restart docker" \
+	| sudo tee /etc/sudoers.d/teamcity
