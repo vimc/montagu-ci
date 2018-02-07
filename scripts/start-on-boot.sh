91adf18c5987faf129abbea658e6ad0cd85fe0b5
Martin Eden <martin.s.eden@gmail.com>
2018-02-07 12:31:51 +0000

    Use sudo to do things, and save real user. Also start.

--- scripts/start-on-boot.sh
+++ scripts/start-on-boot.sh
@@ -2,8 +2,17 @@
 set -ex
 here=$(dirname $(dirname $(realpath $0)))
 target=/etc/systemd/system/montagu-ci.service
+user=$USER
 
-cp $here/scripts/montagu-ci.service $target
-sed -i "s:__PATH__:$here:g" $target
-sed -i "s:__USER__:$USER:g" $target
-systemctl enable montagu-ci
+sudo -s <<EOF
+	set -ex
+	cp $here/scripts/montagu-ci.service $target
+	sed -i "s:__PATH__:$here:g" $target
+	sed -i "s:__USER__:$user:g" $target
+	systemctl enable montagu-ci
+	systemctl start montagu-ci
+EOF
+
+set +x
+echo "Montagu continuous integration should now be running and accessible"
+echo "at port 8111, and should automatically resume after a system boot."
