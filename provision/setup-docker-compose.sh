c94ac04a2c34ae7052d8eace2501675407b778e9
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2018-02-08 11:19:50 +0000

    Upgrade docker compose

--- provision/setup-docker-compose.sh
+++ provision/setup-docker-compose.sh
@@ -2,12 +2,14 @@
 
 set -x
 
+VERSION=1.19.0
+
 if which -a docker-compose > /dev/null; then
     echo "docker-compose is already installed"
 else
     echo "installing docker-compose"
     sudo curl -L \
-         "https://github.com/docker/compose/releases/download/1.13.0/docker-compose-$(uname -s)-$(uname -m)" \
+         "https://github.com/docker/compose/releases/download/$VERSION/docker-compose-$(uname -s)-$(uname -m)" \
          -o /usr/bin/docker-compose
     sudo chmod +x /usr/bin/docker-compose
 fi
