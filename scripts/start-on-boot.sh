113e1e9a44e7d73515933c0d595216ed6311fd62
Martin Eden <martin.s.eden@gmail.com>
2018-02-08 16:12:41 +0000

    Remove need for sudo

--- scripts/start-on-boot.sh
+++ scripts/start-on-boot.sh
@@ -2,16 +2,14 @@
 set -ex
 here=$(dirname $(dirname $(realpath $0)))
 target=/etc/systemd/system/montagu-ci.service
-user=$USER
+read -p "What user should the service run as? " user
 
-sudo -s <<EOF
-	set -ex
-	cp $here/scripts/montagu-ci.service $target
-	sed -i "s:__PATH__:$here:g" $target
-	sed -i "s:__USER__:$user:g" $target
-	systemctl enable montagu-ci
-	systemctl start montagu-ci
-EOF
+set -ex
+cp $here/scripts/montagu-ci.service $target
+sed -i "s:__PATH__:$here:g" $target
+sed -i "s:__USER__:$user:g" $target
+systemctl enable montagu-ci
+systemctl start montagu-ci
 
 set +x
 echo "Montagu continuous integration should now be running and accessible"
